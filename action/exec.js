73487012699a2931a16a51de7ff8f5ec41282b0e
nighca <nighca@live.cn>
2013-09-26 17:00:54 +0800

    add exec action

---
+++ action/exec.js
@@ -0,0 +1,37 @@
+var exec = require('child_process').exec;
+
+var myCli = require('../kit/cli');
+var config = require('../kit/config')();
+var kit = require('../kit');
+
+var action = function(){
+    var root = config.root;
+    if(!root){
+        myCli
+            .begin()
+            .fail('Failed to checkout directory.')
+            .end();
+        output.noRootFault();
+        kit.terminate();
+    }
+
+    var workingDirectory = kit.getWorkingDirectory(root);
+    var cmdArr = Array.prototype.slice.call(arguments, 0, arguments.length - 1);
+    exec(cmdArr.join(' '), {cwd: workingDirectory}, function (error, stdout, stderr) {
+        myCli
+            .begin()
+            .write('Output:')
+            .end();
+        myCli.print(stdout);
+
+        if(error){
+            myCli
+                .begin()
+                .fail('Err:')
+                .end();
+            myCli.print(stderr);
+        }
+    });
+};
+
+module.exports = action;
